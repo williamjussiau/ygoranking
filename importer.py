04155a670f41dcf3bec56c4e2166ef2b9f84fe3e
Pierre BOISSEAU <pierreboisseau21@gmail.com>
2021-05-24 00:29:56 +0200

    Premier Commit Pierre : Ajout de importer.py

---
+++ importer.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 24 00:23:35 2021
+
+@author: pboisseau
+"""
+
+from glicko2 import *
+from ygomanagement import *
+from ygoranking import * 
+from ygostats import *
